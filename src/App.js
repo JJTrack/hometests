de289a9d5265d5f499e170f63c043eac5edd1ed6
Jonathan <rabadunk@gmail.com>
2020-09-06 04:21:44 +1200

    can display data, want to update reactively

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Chart from './components/Chart'
+import { csv } from 'd3'
 import './App.css'
 
-function App () {
-  return (
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      rssi:[],
+      time:[]
+    }
+
+  }
+
+  componentDidMount() {
+
+    csv('data0.csv').then(data => {
+      data.forEach(row => {
+        
+        this.state.rssi.push(row.RSSI);
+        this.state.time.push(row.TIME);
+      })
+    });
+
+  }
+
+  render() {
+   return( 
     <div className='App'>
-      <Chart />
-    </div>
-  )
+      <Chart time={this.state.time} rssi={this.state.rssi}/>
+    </div>)
+  }
 }
 
 export default App
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,38 +2,24 @@ import React, { Component } from 'react'
 import { Bar } from 'react-chartjs-2';
 
 
+
 class Chart extends Component {
 
     constructor (props) {
         super(props);
 
         this.state = {
-            chartData:{
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-                datasets:[
-                  {
-                    label:'Population',
-                    data:[
-                      617594,
-                      181045,
-                      153060,
-                      106519,
-                      105162,
-                      95072
-                    ],
-                    backgroundColor:[
-                      'rgba(255, 99, 132, 0.6)',
-                      'rgba(54, 162, 235, 0.6)',
-                      'rgba(255, 206, 86, 0.6)',
-                      'rgba(75, 192, 192, 0.6)',
-                      'rgba(153, 102, 255, 0.6)',
-                      'rgba(255, 159, 64, 0.6)',
-                      'rgba(255, 99, 132, 0.6)'
-                    ]
-                  }
-                ]
-              }
-        }
+          chartData: {
+              labels: this.props.time,
+              datasets:[
+                {
+                  label:'RSSI',
+                  data:this.props.rssi,
+                  backgroundColor: 'rgba(255, 99, 132, 0.6)'
+                }
+              ]
+            }
+          }
 
     }
 
@@ -42,7 +28,7 @@ class Chart extends Component {
             <div className="chart">
                 <Bar
                     data={this.state.chartData}
-                    options={{ maintainAspectRatio: false }}
+                    options={{ maintainAspectRatio: true, responsive: true }}
                     />
             </div>
         )
